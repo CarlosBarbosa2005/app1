91e7098fd935ec8881a243cb2d16d097fea95909
20211TIIMI0303 <cesbarbosa2005@gmail.com>
2023-03-15 09:08:13 -0300

    comentando o código

--- app/src/main/java/barbosa/souza/eduardo/carlos/app1/MainActivity.java
+++ app/src/main/java/barbosa/souza/eduardo/carlos/app1/MainActivity.java
@@ -7,20 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+//criando class para Main Activity
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //criando o botão de enviar pela ID
         Button btnEnviar = findViewById(R.id.btnEnviar);
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
+            //classe para adicionar a funcionalidade para o botão quando clicado
             public void onClick(View view) {
+                //criando o edittext pela ID
                 EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
+                //criando o String textoDigitado para armazenar o que a pessoa escreveu
                 String textoDigitado = etDigiteAqui.getText().toString();
                 etDigiteAqui.setText("");
+                //criando uma intent
                 Intent i = new Intent(MainActivity.this,NextActivity.class);
                 i.putExtra("Texto", textoDigitado);
                 startActivity(i);
--- app/src/main/java/barbosa/souza/eduardo/carlos/app1/NextActivity.java
+++ app/src/main/java/barbosa/souza/eduardo/carlos/app1/NextActivity.java
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-
+//criando uma classe NextActivity
 public class NextActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setando o content view ao layout da app
         setContentView(R.layout.activity_next);
+        //capturando o intent
         Intent i = getIntent();
+        //pegando o textoDigitado utilizando getStringExtra
         String textoDigitado = i.getStringExtra("Texto");
         TextView tvText = findViewById(R.id.tvTexto);
+        //setando o texto digitado a app
         tvText.setText(textoDigitado);
     }
 }
